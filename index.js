1c31e9e999dfa09a75cd23facdc09b15f1eaf96e
Isiaka Abdulahi <isiakaabd@gmail.com>
2021-04-09 23:59:51 +0100

    adding harmburger effect

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+let menuOpen =false
 const newSlide =()=>{
     const burger =document.querySelector(".burger")
     const navLinks =document.getElementsByClassName("nav-links")[0]
 
 
-burger.addEventListener("click",()=>{
-    console.log(navLinks)
+burger.addEventListener("click", ()=>{
+if(!menuOpen){
+    burger.classList.add("open")
     navLinks.classList.toggle("active") 
+    menuOpen=true
+}else{
+    navLinks.classList.toggle("active") 
+burger.classList.remove("open");
+menuOpen = false
+}
 })
 
 
